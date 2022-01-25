15086292ace8a70c3cf5ab0186aec2612a933022
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-25 23:05:45 +0100

    tested the different primitive data types

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,19 @@
 public class hello {
 
     public static void main(String[] args) {
-        
+        int score = 20;
+        String name = "Clinton";
+        System.out.printf("My name is %s, I'm %d years old\n", name, score);
+
+        int minInt = Integer.MIN_VALUE;
+        int maxInt = Integer.MAX_VALUE;
+        System.out.printf("The Minimum integer is %d and the max integer is %d\n", minInt, maxInt);
+
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+        System.out.printf("The Minimum Byte is %d and the max Byte is %d\n", minByte, maxByte);
+
+
+
     }
 }
